8e17b1ef5fe8938d79a5f27891e51d88171cc222
Harry <henry.iz@yandex.ru>
2024-04-17 20:05:23 +0300

    Added some functionality

---
+++ main.js
@@ -0,0 +1,15 @@
+const array = ["Ivan", "Petrov"];
+
+let [firstName, surname] = array;
+console.log(firstName, surname);
+
+let [Name, sname] = "Iban Gabov".split();
+console.log(Name, sname);
+
+let [a,b,x] = "abx";
+console.log(a, b, x);
+
+
+let [q,,,,w] = [1,2,3,4,5]
+
+console.log(q,w);
